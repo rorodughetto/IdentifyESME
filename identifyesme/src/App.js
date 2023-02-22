470f841222b594441d5fb1613b2cf786e16be698
rorodughetto <romain.eyquem@esme.fr>
2023-02-22 15:58:58 +0100

    Interaction API

--- identifyesme/src/App.js
+++ identifyesme/src/App.js
@@ -1,10 +1,26 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
+
+function sendImageToApi(imageFile) {
+    const apiUrl = 'https://northeurope.api.cognitive.microsoft.com/customvision/v3.0/Prediction/2448c95e-5c9f-4cd8-8f74-7d39ad438349/detect/iterations/Iteration2/image';
+    const predictionKey = 'b995b64f07a340eb8d1956ca2dc4134c';
+    const contentType = 'application/octet-stream';
+
+    const headers = {
+        'Prediction-Key': predictionKey,
+        'Content-Type': contentType
+    };
+
+    const formData = new FormData();
+    formData.append('image', imageFile);
+
+    return axios.post(apiUrl, formData, { headers: headers });
+}
 
 function App() {
     const [image, setImage] = useState(null);
-
-
+    const [predictions, setPredictions] = useState([]);
 
     const handleDrop = (e) => {
         e.preventDefault();
@@ -26,38 +42,60 @@ function App() {
         reader.readAsDataURL(file);
     };
 
+    const handleSubmit = () => {
+        if (image) {
+            sendImageToApi(image)
+                .then(response => {
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+    };
+
+
+
     return ( <
-        div className = "App" >
-        <
-        h1 > Identify ESME < /h1> <
-        div className = "button-container" >
-        <
-        button className = "add-image-btn"
-        onClick = {
-            () => document.getElementById("fileInput").click()
-        } >
-        Add Image <
-        /button> < /
-        div > <
-        div className = "dropzone" > {
-            image ? ( <
-                img src = { image }
-                alt = "uploaded" / >
-            ) : ( <
-                p > Drop image here or click to upload < /p>
+            div className = "App" >
+            <
+            h1 > Drag & Drop or Upload Image < /h1> <
+            div className = "button-container" >
+            <
+            input type = "file"
+            accept = "image/*"
+            onChange = { handleUpload }
+            /> <
+            button onClick = { handleSubmit } > Submit < /button> < /
+            div > <
+            div className = "dropzone"
+            onDragOver = {
+                (e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+            }
+            onDrop = { handleDrop } > {
+                image ? ( <
+                    img src = { image }
+                    alt = "uploaded" / >
+                ) : ( <
+                    p > Drop image here or click to upload < /p>
+                )
+            } <
+            /div> {
+            predictions.length > 0 && ( <
+                div className = "predictions" > {
+                    predictions.map((prediction) => ( <
+                        p key = { prediction.tagName } > { prediction.tagName }: { prediction.probability } <
+                        /p>
+                    ))
+                } <
+                /div>
             )
         } <
-        /div> <
-        input id = "fileInput"
-        type = "file"
-        accept = "image/*"
-        onChange = { handleUpload }
-        /> <
-        button className = "submit-btn" > Submit < /button> </div >
-    );
-
-
-
+        /div>
+);
 }
 
 export default App;
