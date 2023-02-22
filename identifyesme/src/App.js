c2fe99ca6afbfac930e24e45786e77c9121be73a
rorodughetto <1402romainE>
2023-02-22 19:09:38 +0100

    API Sucess

--- identifyesme/src/App.js
+++ identifyesme/src/App.js
@@ -3,19 +3,27 @@ import axios from "axios";
 import "./App.css";
 
 function sendImageToApi(imageFile) {
-    const apiUrl = 'https://northeurope.api.cognitive.microsoft.com/customvision/v3.0/Prediction/2448c95e-5c9f-4cd8-8f74-7d39ad438349/detect/iterations/Iteration2/image';
-    const predictionKey = 'b995b64f07a340eb8d1956ca2dc4134c';
-    const contentType = 'application/octet-stream';
+    const apiUrl = "https://northeurope.api.cognitive.microsoft.com/customvision/v3.0/Prediction/2448c95e-5c9f-4cd8-8f74-7d39ad438349/detect/iterations/Iteration2/image";
+    const predictionKey = "b995b64f07a340eb8d1956ca2dc4134c";
+    const contentType = "application/octet-stream";
+    const imageData = imageFile.replace(/^data:image\/[a-z]+;base64,/, "");
+
+    const binaryData = atob(imageData);
+    const length = binaryData.length;
+    const bytes = new Uint8Array(length);
+    for (let i = 0; i < length; i++) {
+        bytes[i] = binaryData.charCodeAt(i);
+    }
+
 
     const headers = {
-        'Prediction-Key': predictionKey,
-        'Content-Type': contentType
+        "Prediction-Key": predictionKey,
+        "Content-Type": contentType,
     };
-
     const formData = new FormData();
     formData.append('image', imageFile);
 
-    return axios.post(apiUrl, formData, { headers: headers });
+    return axios.post(apiUrl, bytes, { headers: headers });
 }
 
 function App() {
@@ -45,22 +53,22 @@ function App() {
     const handleSubmit = () => {
         if (image) {
             sendImageToApi(image)
-                .then(response => {
+                .then((response) => {
                     console.log(response.data);
                 })
-                .catch(error => {
+                .catch((error) => {
                     console.error(error);
                 });
         }
     };
 
-
-
     return ( <
             div className = "App" >
             <
             h1 > Drag & Drop or Upload Image < /h1> <
-            div className = "button-container" > < button className = "add-image-btn"
+            div className = "button-container" >
+            <
+            button className = "add-image-btn"
             onClick = {
                 () => document.getElementById("fileInput").click()
             } >
@@ -71,7 +79,7 @@ function App() {
             type = "file"
             accept = "image/*"
             onChange = { handleUpload }
-            />   <
+            /> <
             div className = "dropzone"
             onDragOver = {
                 (e) => {
@@ -82,14 +90,20 @@ function App() {
             onDrop = { handleDrop } > {
                 image ? ( <
                     img src = { image }
-                    alt = "uploaded" / >
+                    alt = "uploaded"
+                    style = {
+                        { maxWidth: "100%" }
+                    }
+                    />
                 ) : ( <
                     p > Drop image here or click to upload < /p>
                 )
             } <
-            /div><
+            /div> <
             button className = "submit-btn"
-            onClick = { handleSubmit } > Submit < /button>  {
+            onClick = { handleSubmit } >
+            Submit <
+            /button> {
             predictions.length > 0 && ( <
                 div className = "predictions" > {
                     predictions.map((prediction) => ( <
