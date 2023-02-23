dcdc64993ff42d8b4cc349743f3fc4c4601315df
rorodughetto <romain.eyquem@esme.fr>
2023-02-23 18:01:05 +0100

    Changement classement

--- identifyesme/src/App.js
+++ identifyesme/src/App.js
@@ -8,7 +8,7 @@ import ClipLoader from "react-spinners/ClipLoader";
 
 
 function sendImageToApi(imageFile) {
-    const apiUrl = "https://northeurope.api.cognitive.microsoft.com/customvision/v3.0/Prediction/2448c95e-5c9f-4cd8-8f74-7d39ad438349/detect/iterations/Iteration2/image";
+    const apiUrl = "https://northeurope.api.cognitive.microsoft.com/customvision/v3.0/Prediction/2448c95e-5c9f-4cd8-8f74-7d39ad438349/detect/iterations/Iteration7/image";
     const predictionKey = "b995b64f07a340eb8d1956ca2dc4134c";
     const contentType = "application/octet-stream";
     const imageData = imageFile.replace(/^data:image\/[a-z]+;base64,/, "");
@@ -79,110 +79,114 @@ function App() {
     };
 
 
-    return ( <
-        div className = "App" >
+    return (<body><
+        div className="App" >
         <
-        h1 > Identify ESME < /h1> {isUnderstood ? ( <
-        div className = "body" >
+        h1 > Identify ESME < /h1>{isUnderstood ? (<div className="body" >
 
-        <
-        div className = "button-container" >
-        <
-        button className = "add-image-btn"
-        onClick = {
-            () => document.getElementById("fileInput").click()
-        } >
-        Ajouter image <
+                <
+        div className="button-container" >
+                    <
+        button className="add-image-btn"
+                        onClick={
+                            () => document.getElementById("fileInput").click()
+                        } >
+                        Ajouter image <
         /button> < /
         div > <
-        input id = "fileInput"
-        type = "file"
-        accept = "image/*"
-        onChange = { handleUpload }
-        /> <
-        div className = "container" >
-        <
-        div className = "dropzone"
-        onDragOver = {
-            (e) => {
-                e.preventDefault();
-                e.stopPropagation();
-            }
-        }
-        onDrop = { handleDrop } > {
-            image ? ( <
-                img src = { image }
-                alt = "uploaded"
-                style = {
-                    { maxWidth: "100%" }
-                }
-                />
-            ) : ( <
+                            input id="fileInput"
+                            type="file"
+                            accept="image/*"
+                            onChange={handleUpload}
+                        /> <
+        div className="container" >
+                            <
+        div className="dropzone"
+                                onDragOver={
+                                    (e) => {
+                                        e.preventDefault();
+                                        e.stopPropagation();
+                                    }
+                                }
+                                onDrop={handleDrop} > {
+                                    image ? (<
+                                        img src={image}
+                                        alt="uploaded"
+                                        style={
+                                            { maxWidth: "100%" }
+                                        }
+                                    />
+                                    ) : (<
                 p > Drop image here or click to upload < /p>
-            )
+                                        )
         } <
         /div> {
-        isLoading ? ( <
-            div className = "loading-animation-container" >
-            <
-            ClipLoader color = { "#36D7B7" }
-            loading = { isLoading }
-            size = { 150 }
-            /> < /
+                                            isLoading ? (<
+            div className="loading-animation-container" >
+                                                <
+                                                    ClipLoader color={"#36D7B7"}
+                                                    loading={isLoading}
+                                                    size={150}
+                                                /> < /
             div >
-        ) : null
+                                                ) : null
     } {
-        predictions.length > 0 ? ( <
-            div className = "predictions" >
-            <
-            table >
-            <
-            thead >
-            <
-            tr >
-            <
-            th > Logo < /th> <
-            th > Probability < /th> < /
-            tr > <
-            /thead> <
-            tbody > {
-                predictions.slice(0, 3).map((prediction) => ( <
-                    tr key = { prediction.tagId } >
-                    <
-                    td > { prediction.tagName.charAt(0).toUpperCase() + prediction.tagName.slice(1) } < /td> <
-                    td > {
-                        (prediction.probability * 100).toFixed(2)
-                    } % < /td> < /
-                    tr >
-                ))
-            } <
-            /tbody> < /
-            table > <
-            /div>
-        ) : null
-    } < /
-    div > <
-        div className = "button-submit-container" >
-        <
-        button className = "submit-btn"
-    onClick = { handleSubmit } >
-        Soumettre <
+                                                    predictions.length > 0 && ( // vérifie si predictions n'est pas vide
+                                                        <div className="predictions">
+                                                            {
+                                                                predictions.some(p => p.probability >= 0.7) ? ( // vérifie si au moins une probabilité est supérieure ou égale à 60%
+                                                                    <table>
+                                                                        <thead>
+                                                                            <tr>
+                                                                                <th>Logo</th>
+                                                                                <th>Probability</th>
+                                                                            </tr>
+                                                                        </thead>
+                                                                        <tbody>
+                                                                            {
+                                                                                predictions
+                                                                                    .filter(p => p.probability >= 0.7) // filtre les prédictions dont la probabilité est supérieure ou égale à 70%
+
+                                                                                    .map(prediction => (
+                                                                                        <tr key={prediction.tagId}>
+                                                                                            <td>{prediction.tagName.charAt(0).toUpperCase() + prediction.tagName.slice(1)}</td>
+                                                                                            <td>{(prediction.probability * 100).toFixed(2)}%</td>
+                                                                                        </tr>
+                                                                                    ))
+                                                                            }
+                                                                        </tbody>
+                                                                    </table>
+                                                                ) : (
+                                                                    <p>Logo non détécté</p> // affiche "pas de logo détecté" si aucune probabilité n'est supérieure ou égale à 60%
+                                                                )
+                                                            }
+                                                        </div>
+                                                    )
+
+                                                } < /    div > <
+        div className="button-submit-container" >
+                                                    <
+        button className="submit-btn"
+                                                        onClick={handleSubmit} >
+                                                        Soumettre <
         /button> < /div > < /div > ) : (<div className="understood-container">  <
-    p class = "typewriter" > L 'interface Web que nous avons créée  permet de détecter des logos dans des images pour des entreprises comme Apple, Microsoft, Google, Nvidia et Intel grâce à une API de reconnaissance visuelle entraînée par le machine learning. Les utilisateurs peuvent facilement télécharger une image ou la faire glisser et la déposer dans la zone de dépôt prévue à cet effet. En appuyant sur le bouton de soumission, l'
-    API analyse l 'image et renvoie les prédictions de logos détectés, avec leur probabilité respective. Les résultats sont affichés à l'
-    utilisateur sous forme de tableau, avec les noms de logo et les probabilités correspondantes.L 'interface est simple à utiliser et permet une reconnaissance rapide et précise des logos recherchés.</p> <
-    button className = "understood-btn"
-    onClick = { handleUnderstood } >
-        C 'est compris < /
+                                                            p class="typewriter" >Nous avons créé une interface Web qui permet de détecter des logos dans des images pour des entreprises comme Apple, Microsoft, Google, Nvidia, Samsung ainsi qu'Intel. Cette détection est réalisée grâce à une API de reconnaissance visuelle entraînée par le machine learning.
+
+                                                            Les utilisateurs peuvent facilement télécharger une image ou la faire glisser et la déposer dans la zone de dépôt prévue à cet effet. En appuyant sur le bouton de soumission, l'API analyse l'image et renvoie les prédictions de logos détectés, avec leur probabilité respective.
+
+                                                            Les résultats sont affichés à l'utilisateur sous forme de tableau, avec les noms de logo et les probabilités correspondantes. Cette interface est simple à utiliser et permet une reconnaissance rapide et précise des logos recherchés.</p> <
+                                                                                                                    button className="understood-btn"
+                                                                onClick={handleUnderstood} >
+                                                                C'est compris < /
     button > <
         /div>
-)
-} <
-div class = "container-footer" >
-    <
-    p > Application produite par Farah Dourouni, Mehdi Lallouche, Rami Moulla, Yvan Gunewou - Takam et Romain Eyquem dans le cadre du 5 days challenge à ESME Sudria < /p> < /
+                                                                )
+}  <
+div class="container-footer" >
+                                                                    <
+                                                                        p > Application produite par Farah Dourouni, Mehdi Lallouche, Rami Moulla, Yvan Gunewou-Takam et Romain Eyquem dans le cadre du 5 days challenge à ESME Sudria < /p> < /
     div > < /
-div >
-);
+div ></body>
+                                                                    );
 }
-export default App;
+                                                                    export default App;
