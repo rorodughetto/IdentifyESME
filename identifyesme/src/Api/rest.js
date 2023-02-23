d70bd253c8ac494b8b62710e7c2255eea8323d4a
Yvan GUNEWOU <yvangunewou@gmail.com>
2023-02-23 19:57:58 +0100

    Affichage des rectangles de détection de logo

--- identifyesme/src/Api/rest.js
+++ identifyesme/src/Api/rest.js
@@ -1,13 +1,30 @@
-fetch('https://northeurope.api.cognitive.microsoft.com/customvision/v3.0/Prediction/2448c95e-5c9f-4cd8-8f74-7d39ad438349/detect/iterations/Iteration2/url', {
-        method: 'POST',
-        headers: {
-            'Prediction-Key': 'b995b64f07a340eb8d1956ca2dc4134c',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            Url: 'https://example.com/image.png'
-        })
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error))
+import axios from 'axios';
+
+const apiUrl = "https://northeurope.api.cognitive.microsoft.com/customvision/v3.0/Prediction/2448c95e-5c9f-4cd8-8f74-7d39ad438349/detect/iterations/Iteration7/image";
+
+const rest = {
+	sendImageToApi(imageFile) {
+		const apiUrl = 'https://northeurope.api.cognitive.microsoft.com/customvision/v3.0/Prediction/2448c95e-5c9f-4cd8-8f74-7d39ad438349/detect/iterations/Iteration2/image';
+		const predictionKey = 'b995b64f07a340eb8d1956ca2dc4134c';
+		const contentType = 'application/octet-stream';
+		const imageData = imageFile.replace(/^data:image\/[a-z]+;base64,/, '');
+
+		const binaryData = atob(imageData);
+		const length = binaryData.length;
+		const bytes = new Uint8Array(length);
+		for (let i = 0; i < length; i++) {
+			bytes[i] = binaryData.charCodeAt(i);
+		}
+
+		const headers = {
+			'Prediction-Key': predictionKey,
+			'Content-Type': contentType,
+		};
+		const formData = new FormData();
+		formData.append('image', imageFile);
+
+		return axios.post(apiUrl, bytes, { headers: headers });
+	},
+};
+
+export default rest;
--- identifyesme/src/App.js
+++ identifyesme/src/App.js
@@ -1,160 +1,167 @@
-import React, { useState } from "react";
-import axios from "axios";
-import "./App.css";
-import { css } from "@emotion/react";
-import ClipLoader from "react-spinners/ClipLoader";
-
-
-
-
-function sendImageToApi(imageFile) {
-    const apiUrl = "https://northeurope.api.cognitive.microsoft.com/customvision/v3.0/Prediction/2448c95e-5c9f-4cd8-8f74-7d39ad438349/detect/iterations/Iteration2/image";
-    const predictionKey = "b995b64f07a340eb8d1956ca2dc4134c";
-    const contentType = "application/octet-stream";
-    const imageData = imageFile.replace(/^data:image\/[a-z]+;base64,/, "");
-
-    const binaryData = atob(imageData);
-    const length = binaryData.length;
-    const bytes = new Uint8Array(length);
-    for (let i = 0; i < length; i++) {
-        bytes[i] = binaryData.charCodeAt(i);
-    }
-
-
-    const headers = {
-        "Prediction-Key": predictionKey,
-        "Content-Type": contentType,
-    };
-    const formData = new FormData();
-    formData.append('image', imageFile);
-
-    return axios.post(apiUrl, bytes, { headers: headers });
-}
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import { css } from '@emotion/react';
+import ClipLoader from 'react-spinners/ClipLoader';
+import rest from './Api/rest';
 
 function App() {
-    const [image, setImage] = useState(null);
-    const [predictions, setPredictions] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
+	const [image, setImage] = useState(null);
+	const [predictions, setPredictions] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
+	const handleDrop = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		const file = e.dataTransfer.files[0];
+		const reader = new FileReader();
+		reader.onload = () => {
+			setImage(reader.result);
+		};
+		reader.readAsDataURL(file);
+	};
 
-    const handleDrop = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        const file = e.dataTransfer.files[0];
-        const reader = new FileReader();
-        reader.onload = () => {
-            setImage(reader.result);
-        };
-        reader.readAsDataURL(file);
-    };
+	const handleUpload = (e) => {
+		const file = e.target.files[0];
+		const reader = new FileReader();
+		reader.onload = () => {
+			setImage(reader.result);
+		};
+		reader.readAsDataURL(file);
+	};
 
-    const handleUpload = (e) => {
-        const file = e.target.files[0];
-        const reader = new FileReader();
-        reader.onload = () => {
-            setImage(reader.result);
-        };
-        reader.readAsDataURL(file);
-    };
+    const removeBoundingBoxs = () => {
+        predictions.forEach((prediction, index) => {
+            const boundingBoxDiv = document.getElementById('bounding-box' + index);
+            if (boundingBoxDiv) boundingBoxDiv.remove();
+        })
+    }
 
-    const handleSubmit = () => {
-        if (image) {
-            setIsLoading(true);
-            setPredictions([]);
-            sendImageToApi(image)
-                .then((response) => {
-                    setPredictions(response.data.predictions);
-                })
-                .catch((error) => {
-                    console.error(error);
-                })
-                .finally(() => setIsLoading(false));
-        }
-    };
+	const createBoundingBox = () => {
+		const boundingBoxColors = ['red', 'green', 'blue', 'yellow', 'orange', 'purple', 'pink', 'brown', 'black', 'white'];
+		const dropzone = document.getElementById('dropzone');
+        new ResizeObserver(() => {
+            removeBoundingBoxs();
+			for (let index = 0; index < predictions.length; index++) {
+                if (predictions[index].probability > 0.6) {
+					const image = document.getElementById('image');
+					const imageRect = image.getBoundingClientRect();
+					const imageWidth = imageRect.width;
+					const imageHeight = imageRect.height;
+					const imageLeft = imageRect.left;
+					const imageTop = imageRect.top;
+					const boundingBox = document.createElement('div');
+					boundingBox.id = 'bounding-box' + index;
+					boundingBox.style.border = '3px solid ' + boundingBoxColors[index];
+					boundingBox.style.height = imageHeight * predictions[index].boundingBox.height + 'px';
+					boundingBox.style.width = imageWidth * predictions[index].boundingBox.width + 'px';
+					boundingBox.style.position = 'absolute';
+					boundingBox.style.top = imageTop + predictions[index].boundingBox.top * imageHeight + 'px';
+					boundingBox.style.left = imageLeft + predictions[index].boundingBox.left * imageWidth + 'px';
+					document.body.appendChild(boundingBox);
+				}
+			}
+		}).observe(dropzone);
+        window.addEventListener('resize', () => {
+            removeBoundingBoxs();
+			for (let index = 0; index < predictions.length; index++) {
+				if (predictions[index].probability > 0.6) {
+					const image = document.getElementById('image');
+					const imageRect = image.getBoundingClientRect();
+					const imageWidth = imageRect.width;
+					const imageHeight = imageRect.height;
+					const imageLeft = imageRect.left;
+					const imageTop = imageRect.top;
+					const boundingBox = document.createElement('div');
+					boundingBox.id = 'bounding-box' + index;
+					boundingBox.style.border = '3px solid ' + boundingBoxColors[index];
+					boundingBox.style.height = imageHeight * predictions[index].boundingBox.height + 'px';
+					boundingBox.style.width = imageWidth * predictions[index].boundingBox.width + 'px';
+					boundingBox.style.position = 'absolute';
+					boundingBox.style.top = imageTop + predictions[index].boundingBox.top * imageHeight + 'px';
+					boundingBox.style.left = imageLeft + predictions[index].boundingBox.left * imageWidth + 'px';
+					document.body.appendChild(boundingBox);
+				}
+			}
+		});
+	};
 
+	const handleSubmit = () => {
+		if (image) {
+			setIsLoading(true);
+			setPredictions([]);
+			rest
+				.sendImageToApi(image)
+				.then((response) => {
+					setPredictions(response.data.predictions);
+				})
+				.catch((error) => {
+					console.error(error);
+				})
+				.finally(() => {
+					setIsLoading(false);
+				});
+		}
+	};
 
-    return ( <
-        div className = "App" >
-        <
-        h1 > Drag & Drop or Upload Image < /h1> <
-        div className = "button-container" >
-        <
-        button className = "add-image-btn"
-        onClick = {
-            () => document.getElementById("fileInput").click() } >
-        Add Image <
-        /button> <
-        /div> <
-        input id = "fileInput"
-        type = "file"
-        accept = "image/*"
-        onChange = { handleUpload }
-        /> <
-        div className = "container" >
-        <
-        div className = "dropzone"
-        onDragOver = {
-            (e) => {
-                e.preventDefault();
-                e.stopPropagation();
-            }
-        }
-        onDrop = { handleDrop } > {
-            image ? ( <
-                img src = { image }
-                alt = "uploaded"
-                style = {
-                    { maxWidth: "100%" } }
-                />
-            ) : ( <
-                p > Drop image here or click to upload < /p>
-            )
-        } <
-        /div> {
-            isLoading ? ( <
-                div className = "loading-animation-container" >
-                <
-                ClipLoader color = { "#36D7B7" }
-                loading = { isLoading }
-                size = { 150 }
-                /> <
-                /div>
-            ) : null
-        } {
-            predictions.length > 0 ? ( <
-                div className = "predictions" >
-                <
-                table >
-                <
-                thead >
-                <
-                tr >
-                <
-                th > Logo < /th> <
-                th > Probability < /th> <
-                /tr> <
-                /thead> <
-                tbody > {
-                    predictions.slice(0, 3).map((prediction) => ( <
-                        tr key = { prediction.tagId } >
-                        <
-                        td > { prediction.tagName.charAt(0).toUpperCase() + prediction.tagName.slice(1) } < /td> <
-                        td > {
-                            (prediction.probability * 100).toFixed(2) } % < /td> <
-                        /tr>
-                    ))
-                } <
-                /tbody> <
-                /table> <
-                /div>
-            ) : null
-        } <
-        /div> <
-        button className = "submit-btn"
-        onClick = { handleSubmit } >
-        Submit <
-        /button> <
-        /div>
-    );
+	return (
+		<div className='App'>
+			{!isLoading && predictions.length > 0 && createBoundingBox()}
+			<h1> Drag & Drop or Upload Image </h1>{' '}
+			<div className='button-container'>
+				<button className='add-image-btn' onClick={() => document.getElementById('fileInput').click()}>
+					Add Image{' '}
+				</button>{' '}
+			</div>{' '}
+			<input id='fileInput' type='file' accept='image/*' onChange={handleUpload} />{' '}
+			<div className='container'>
+				<div
+					id='dropzone'
+					className='dropzone'
+					onDragOver={(e) => {
+						e.preventDefault();
+						e.stopPropagation();
+					}}
+					onDrop={handleDrop}
+				>
+					{' '}
+					{image ? (
+						<div>
+							<img id='image' src={image} alt='uploaded' style={{ width: '100%', height: '100%' }} />
+						</div>
+					) : (
+						<p> Drop image here or click to upload </p>
+					)}{' '}
+				</div>{' '}
+				{/* {isLoading ? (
+						<div id='spinner' className='loading-animation-container'>
+							<ClipLoader color={'#36D7B7'} loading={isLoading} size={150} />{' '}
+						</div>
+					) : null}{' '} */}
+				{predictions.length > 0 ? (
+					<div className='predictions'>
+						<table>
+							<thead>
+								<tr>
+									<th> Logo </th> <th> Probability </th>{' '}
+								</tr>{' '}
+							</thead>{' '}
+							<tbody>
+								{' '}
+								{predictions.slice(0, 3).map((prediction) => (
+									<tr key={prediction.tagId}>
+										<td> {prediction.tagName.charAt(0).toUpperCase() + prediction.tagName.slice(1)} </td> <td> {(prediction.probability * 100).toFixed(2)} % </td>{' '}
+									</tr>
+								))}{' '}
+							</tbody>{' '}
+						</table>{' '}
+					</div>
+				) : null}{' '}
+			</div>{' '}
+			<button className='submit-btn' onClick={handleSubmit}>
+				Submit{' '}
+			</button>{' '}
+		</div>
+	);
 }
 export default App;
